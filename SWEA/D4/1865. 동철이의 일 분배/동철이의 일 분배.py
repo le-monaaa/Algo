e43af67d80fbf967f79e03b81d20cdbb8562b340
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2023-09-19 19:43:00 +0900

    [D4] Title: 동철이의 일 분배, Time: 29,460 ms, Memory: 79,504 KB -BaekjoonHub

---
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
@@ -0,0 +1,51 @@
+# SWEA 1865 동철이의 일 분배
+# N명의 직원 N개의 일. i번 직원이 j번 일을 하면 성공할 확률
+# 주어진 일이 모두 성공할 확률의 최댓값
+# T/N/N lines N values
+
+# SWEA 1865 동철이의 일 분배
+# N명의 직원 N개의 일. i번 직원이 j번 일을 하면 성공할 확률
+# 주어진 일이 모두 성공할 확률의 최댓값
+# T/N/N lines N values
+
+def backTracking(row, col, now_percentage, a):
+    global max_percentage
+    # 종료
+    if row == N:
+        if max_percentage < now_percentage:
+            max_percentage = now_percentage
+
+        return
+
+    # 반복
+    for i in range(N):
+        if i not in col:
+            temp = now_percentage
+            if len(task[row][i]) == 3:
+                percentage = 1
+            elif len(task[row][i]) == 1:
+                percentage = float("0.0" + task[row][i])
+            else:
+                percentage = float("0." + task[row][i])
+            now_percentage *= percentage
+            if now_percentage > max_percentage:
+                col.append(i)
+                a.append(task[row][i])
+                backTracking(row + 1, col, now_percentage, a)
+                col.remove(i)
+                a.remove(task[row][i])
+            now_percentage = temp
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input()) # 직원수/업무의수
+    task = []
+    for n in range(N):
+        task.append(list(input().split()))
+
+    max_percentage = 0
+
+    backTracking(0, [], 1, [])
+
+    print(f"#{tc}", "{:.6f}".format(round(max_percentage * 100, 6)))
