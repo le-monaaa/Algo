de4e0ab4e0e6b79f03e6eb34656782e0cee87c92
le-monaaa <101534897+le-monaaa@users.noreply.github.com>
2023-11-07 22:39:50 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 596 ms, Memory: 60032 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,29 @@
+from sys import stdin
+input = stdin.readline
+
+def dfs(graph, start):
+    global count
+    stack = []
+    visited = [0 for _ in range(N+1)]
+    stack.append(start)
+    while stack:
+        now = stack.pop()
+        if not visited[now]:
+            visited[now] = count
+            count += 1
+            stack.extend(graph[now])
+    return visited
+
+N, M, R = map(int, input().split()) # N: 정점 수 M: 간선 수, R: 시작 정점
+arr = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split()) # 양방향 간선
+    arr[u].append(v)
+    arr[v].append(u)
+for n in arr:
+    n.sort()
+count = 1
+visited = dfs(arr, R)
+
+for i in range(1, N+1):
+    print(visited[i])
